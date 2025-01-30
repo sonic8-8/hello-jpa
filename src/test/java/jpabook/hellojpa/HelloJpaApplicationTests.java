c22913d2b9a2fc73481702b19c59a268dff23c30
이승환 <seaung13@gmail.com>
2025-01-30 23:59:57 +0900

    initial commit

---
+++ src/test/java/jpabook/hellojpa/HelloJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.hellojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
